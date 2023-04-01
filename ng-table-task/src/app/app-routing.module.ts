19b8f3c114e5786c84c3550fb79ec75605449a04
sebastian-sobik <sebasob749@student.polsl.pl>
2023-04-01 16:47:40 +0200

    added routing

--- ng-table-task/src/app/app-routing.module.ts
+++ ng-table-task/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Page404Component} from "./page404/page404.component";
+import {MainComponent} from "./main/main.component";
+import {ViewsComponent} from "./views/views.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent},
+  {path: 'views', component: ViewsComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- ng-table-task/src/app/app.module.ts
+++ ng-table-task/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { ViewsComponent } from './views/views.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    ViewsComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
