3396f74c0e951bdfb7532410e79e57545ce5718b
sebastian-sobik <sebasob749@student.polsl.pl>
2023-04-05 22:50:04 +0200

    Refactored: row component

--- ng-table-task/src/app/main/table/row/row.component.ts
+++ ng-table-task/src/app/main/table/row/row.component.ts
@@ -5,23 +5,16 @@ import {Subscription} from "rxjs";
 import {SelectedUsersService} from "../../../selected-users.service";
 import {UserDataService} from "../../../user-data.service";
 
-type ngClass = Record<string, boolean>;
-
 @Component({
   selector: 'app-row',
   templateUrl: './row.component.html',
   styleUrls: ['./row.component.scss']
 })
 export class RowComponent implements OnInit, OnDestroy {
-  subscription: Subscription | undefined;
   @Output() rowCheckboxed: EventEmitter<number> = new EventEmitter<number>();
   @Output() rowDiscarded: EventEmitter<number> = new EventEmitter<number>();
-  ngClasses: Record<string, ngClass> = {
-    menuClass: {},
-    checkboxClass: {},
-    rowClass: {}
-  }
   @Input() user!: User;
+  subscription: Subscription | undefined;
   selected = false;
   toggled = false;
 
@@ -31,9 +24,8 @@ export class RowComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.refreshStyling();
     this.subscription = this.selectedUsersService.onCleared$.subscribe(
-      () => (this.selected) ? this.onSelect() : null
+      () => (this.selected) ? this.select() : null
     )
   }
 
@@ -41,12 +33,8 @@ export class RowComponent implements OnInit, OnDestroy {
     this.subscription?.unsubscribe();
   }
 
-  onClicked($event : Event) {
-    $event.preventDefault();
-    this.onSelect();
-  }
-
-  onSelect() {
+  select($event? : Event) {
+    $event?.preventDefault();
     this.selected = !this.selected;
 
     if (this.selected) {
@@ -54,51 +42,12 @@ export class RowComponent implements OnInit, OnDestroy {
     } else {
       this.rowDiscarded.emit(this.user.id);
     }
-
-    this.refreshStyling();
   }
 
-  onEdit() {
-    this.toggled = false;
-    this.refreshStyling();
+  edit() {
   }
 
-  onDelete() {
-    this.toggled = false;
-    this.refreshStyling();
+  delete() {
     this.usersService.removeUser(this.user);
-  }
-
-  onMenuSelect() {
-    this.toggled = !this.toggled;
-    this.refreshStyling()
-  }
-
-  private refreshStyling() {
-    this.setCheckboxClasses();
-    this.setMenuClasses();
-    this.setRowClasses();
-  }
-
-  setCheckboxClasses() {
-    this.ngClasses['checkboxClass'] = {
-      'bi': true,
-      'bi-square': !this.selected,
-      'bi-check-square-fill': this.selected
-    }
-  }
-
-  setMenuClasses() {
-    this.ngClasses['menuClass'] = {
-      'menu-options': true,
-      'visible': this.toggled
-    }
-  }
-
-  setRowClasses() {
-    this.ngClasses['rowClass'] = {
-      'row': true,
-      'row-selected': this.selected,
-    }
   }
 }
