b7ce0f14a78ad777d241f14291f02269ae91bba6
sebastian-sobik <sebasob749@student.polsl.pl>
2023-04-10 14:55:07 +0200

    Created views page.

--- src/app/views/views.component.ts
+++ src/app/views/views.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserDataService} from "../user-data.service";
+import {User} from "../shared/user.model";
 
 @Component({
   selector: 'app-views',
   templateUrl: './views.component.html',
   styleUrls: ['./views.component.scss']
 })
-export class ViewsComponent {
+export class ViewsComponent implements OnInit{
+  data : User[] | undefined;
 
+  constructor(private usersService: UserDataService) {
+  }
+
+  ngOnInit() {
+    this.data = this.usersService.getUsers();
+  }
 }
